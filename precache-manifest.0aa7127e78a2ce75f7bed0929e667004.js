a90a0637b49a3e6ce89df3eb1f18538efdbeee4c
ailgup <ailgup@mail.com>
2022-06-12 21:40:14 -0400

    Updates

---
+++ precache-manifest.0aa7127e78a2ce75f7bed0929e667004.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "779eea06896315945b7519ceb2c777f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec76329a780e4e9ae646",
+    "url": "/static/css/main.92dc7df6.chunk.css"
+  },
+  {
+    "revision": "6276991dcaca36b702f0",
+    "url": "/static/js/2.792561a8.chunk.js"
+  },
+  {
+    "revision": "472d55e04009af5e2e8677a4dab353c3",
+    "url": "/static/js/2.792561a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec76329a780e4e9ae646",
+    "url": "/static/js/main.2e2a2c15.chunk.js"
+  },
+  {
+    "revision": "f08a05241d8a20afc1dd",
+    "url": "/static/js/runtime-main.cff04c13.js"
+  }
+]);
